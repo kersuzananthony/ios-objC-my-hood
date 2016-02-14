7f5f73a7b6e58d0496388d22464d6db05352ed56
Kersuzan <kersuzan.anthony@gmail.com>
2016-02-14 13:45:44 +0800

    Initial Commit

---
+++ MyHood/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyHood
+//
+//  Created by Kersuzan on 14/02/16.
+//  Copyright © 2016 Kersuzan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyHood/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyHood
+//
+//  Created by Kersuzan on 14/02/16.
+//  Copyright © 2016 Kersuzan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
